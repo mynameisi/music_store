229418c74275e63ed665846a57f1ce0ae9d48c0d
mynameisi <test@nowhere.com>
2013-08-04 18:43:33 +0800

    在IO.java中添加了 System.out.println("The path for this webapp is:\n"+webAppLoc);

--- src/ms/helper/IO.java
+++ src/ms/helper/IO.java
@@ -6,11 +6,15 @@ package ms.helper;
 import java.io.File;
 import java.net.URL;
 
+import background.MyBean;
+
 public class IO {
 	public static String getWebAppFolder(Class cls) {
 		URL r = cls.getClassLoader().getResource(File.separator);
 	    String filePath = r.getFile();
 	    String webAppLoc=new File(filePath).getParentFile().getParent();
+	    
+
 		return webAppLoc;
 	}
 
